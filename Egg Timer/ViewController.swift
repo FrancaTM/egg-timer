6daa4734ec6a073ed4bfb90988a8d6348ef2506f
FrancaTM <francatm@outlook.com>
2020-10-05 12:13:24 -0300

    implement countdown timer (challenge)

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 12]
+    
+    var counter = 0
+    
+    let eggTimes = ["Soft": 300, "Medium": 480, "Hard": 720]
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
@@ -16,13 +19,29 @@ class ViewController: UIViewController {
         switch hardness {
         case "Soft":
             print(eggTimes[hardness]!)
+            counter = eggTimes[hardness]!
         case "Medium":
             print(eggTimes[hardness]!)
+            counter = eggTimes[hardness]!
         case "Hard":
             print(eggTimes[hardness]!)
+            counter = eggTimes[hardness]!
 
         default:
             print("Error")
         }
+        
+        counter = 10
+                
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.counter > 0 {
+                print("\(self.counter) seconds")
+                self.counter -= 1
+            } else {
+                Timer.invalidate()
+                print("timer invalidated")
+            }
+        }
     }
+    
 }
